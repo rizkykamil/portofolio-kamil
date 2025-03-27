e6d85ae9b906ac082d01c863b8e2c7bb67d08168
rizkykamil <rabbaniikamil@gmail.com>
2025-03-27 23:37:56 +0700

    Refactor scrollingInfo component to use Next.js Link for navigation

--- app/components/scrollingInfo.tsx
+++ app/components/scrollingInfo.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function ScrollingInfo() {
     return (
         <div className="work-together-slider">
             <div className="slider-main d-flex gap-4 align-items-center">
                 <div className="slider-item">
-                    <a href="/contact">Let&apos;s 👋 Work Together</a>
-                    <a href="/contact">Let&apos;s 👋 Work Together</a>
+                    <Link href="/contact">Let&apos;s 👋 Work Together</Link>
+                    <Link href="/contact">Let&apos;s 👋 Work Together</Link>
                 </div>
                 <div className="slider-item">
-                    <a href="/contact">Let&apos;s 👋 Work Together</a>
-                    <a href="/contact">Let&apos;s 👋 Work Together</a>
+                    <Link href="/contact">Let&apos;s 👋 Work Together</Link>
+                    <Link href="/contact">Let&apos;s 👋 Work Together</Link>
                 </div>
             </div>
         </div>
