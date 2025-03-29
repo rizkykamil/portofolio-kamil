55d8a36ca4a3fe7bcef7ef0a9982e540714f6e3b
rizkykamil <rabbaniikamil@gmail.com>
2025-03-30 03:21:33 +0700

    Refactor database connection to use environment variables and add dotenv support

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,13 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
 
+dotenv.config()
+// sesuai degnan env
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'portofolio',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 })
 
 export default db
