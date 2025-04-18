3ba576714d2b8b4fb3923617bc4ad982ad0b100a
rizkykamil <rabbaniikamil@gmail.com>
2025-04-19 04:38:29 +0700

    Update database connection to use environment variables

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,15 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'portofolio',
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: Number(process.env.DB_PORT), // Convert string to number
+});
+
 
 export default db
